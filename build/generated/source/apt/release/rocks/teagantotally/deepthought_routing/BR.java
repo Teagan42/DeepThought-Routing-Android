1c18a1e3a0d2a8d034032d74b5c01edf60158ca4
Teagan Glenn <tglenn@Teagans-MBP.int.teagantotally.rocks>
2017-10-02 21:55:29 -0600

    Remove build from git

--- build/generated/source/apt/release/rocks/teagantotally/deepthought_routing/BR.java
+++
@@ -1,9 +0,0 @@
-package rocks.teagantotally.deepthought_routing;
-
-public class BR {
-        public static int _all = 0;
-        public static int param = 1;
-        public static int permission = 2;
-        public static int route = 3;
-        public static int routes = 4;
-}
